639f3fd50c49b61d1c19036f223472f4a6b0c65b
Ayon Bhowmick <ayb224@lehigh.edu>
2023-01-20 14:24:28 -0500

    connect

--- portfolio/src/pages/connect.js
+++ portfolio/src/pages/connect.js
@@ -3,8 +3,34 @@ import "../styles/global.scss"
 import Title from "./title"
 import "../styles/connect.scss"
 
+const Button = ({ text, link }) => {
+  return (
+    <a className="btn" href={link} target="_blank" rel="noopener noreferrer">
+      <p>{text}</p>
+    </a>
+  )
+}
+
+const openLink = (link) => {
+  return async () => {
+    await window.open(link, "_blank")
+  }
+}
+
 
 const Connect = () => {
+  const downloadResume = () => {
+    fetch("AyonBhowmick-Resume.pdf").then(res => {
+      res.blob().then(blob => {
+        const url = window.URL.createObjectURL(new Blob([blob]));
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "AyonBhowmick-Resume.pdf";
+        link.click();
+      })
+    })
+  }
+
   return (
     <>
       <div className="body">
@@ -13,12 +39,26 @@ const Connect = () => {
         </div>
         <div className="info-container">
           <div className="conBox">
-            <button className="btn">Resume</button>
-            <p className="conText">AyonBhowmick-Resume.pdf</p>
-            <button className="btn">Email</button>
-            <button className="btn">LinkedIn</button>
-            <button className="btn">GitHub</button>
-            <button className="btn">DevPost</button>
+            <div className="conElement">
+              <button className="btn" onClick={downloadResume}>Resume</button>
+              <p>AyonBhowmick-Resume.pdf</p>
+            </div>
+            <div className="conElement">
+              <button className="btn" onClick={openLink("mailto:ayonbhowmick2002@gmail.com")}>Email</button>
+              <p>ayonbhowmick2002@gmail.com</p>
+            </div>
+            <div className="conElement">
+              <button className="btn" onClick={openLink("https://www.linkedin.com/in/ayon-bhowmick-40685a226/")}>LinkedIn</button>
+              <p>linkedin.com/in/ayon-bhowmick-40685a226/</p>
+            </div>
+            <div className="conElement">
+              <button className="btn" onClick={openLink("https://github.com/Ayon-Bhowmick")}>Github</button>
+              <p>github.com/Ayon-Bhowmick</p>
+            </div>
+            <div className="conElement">
+              <button className="btn" onClick={openLink("https://devpost.com/ayonbolt")}>DevPost</button>
+              <p>devpost.com/ayonbolt</p>
+            </div>
           </div>
         </div>
       </div>
