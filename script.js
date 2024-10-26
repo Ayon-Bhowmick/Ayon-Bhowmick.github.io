aedaf9da60efab0b399e66334d25f6f0fc3f8ab4
Ayon Bhowmick <ayonbhowmick2002@gmail.com>
2024-10-26 16:37:57 -0400

    some bs

--- script.js
+++ script.js
@@ -2,3 +2,20 @@ const themeToggle = document.getElementById('themeToggle');
 themeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const greetingElement = document.getElementById("greeting");
+    const currentHour = new Date().getHours();
+
+    let greeting;
+
+    if (currentHour < 12) {
+        greeting = "Good Morning";
+    } else if (currentHour < 18) {
+        greeting = "Good Afternoon";
+    } else {
+        greeting = "Good Evening";
+    }
+
+    greetingElement.textContent = greeting;
+});
